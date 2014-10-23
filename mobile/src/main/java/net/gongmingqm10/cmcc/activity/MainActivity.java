3090713b1f1e52d6a1fde469bde6405ebac2b1ca
gongming <gongmingqm10@foxmail.com>
2014-10-23 17:39:07 +0800

    Change Map search to Google map Navigation

--- mobile/src/main/java/net/gongmingqm10/cmcc/activity/MainActivity.java
+++ mobile/src/main/java/net/gongmingqm10/cmcc/activity/MainActivity.java
@@ -1,19 +1,11 @@
 package net.gongmingqm10.cmcc.activity;
 
-import android.app.PendingIntent;
-import android.content.Intent;
-import android.net.Uri;
-import android.support.v4.app.NotificationCompat;
-import android.support.v4.app.NotificationManagerCompat;
-import android.support.v4.app.RemoteInput;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
+import android.webkit.WebView;
 
 import net.gongmingqm10.cmcc.R;
-import net.gongmingqm10.cmcc.utils.Constant;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -22,8 +14,8 @@ import cn.jpush.android.api.InstrumentedActivity;
 
 public class MainActivity extends InstrumentedActivity {
 
-    @InjectView(R.id.pushBtn)
-    Button pushBtn;
+    @InjectView(R.id.webView)
+    WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,50 +26,9 @@ public class MainActivity extends InstrumentedActivity {
     }
 
     private void init() {
-        pushBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startNotification();
-            }
-        });
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("http://www.baidu.com/");
     }
-
-    private void startNotification() {
-        int notificationID = 100;
-        Intent notificationIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://10086.cn/sn/"));
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-
-        Intent actionIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:0,0?q=中国移动"));
-        PendingIntent actionPendingIntent = PendingIntent.getActivity(this, 0, actionIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-
-
-        String[] replyChoices = new String[] {Constant.CMD_YES, Constant.CMD_NO, Constant.CMD_MAYBE};
-        RemoteInput remoteInput = new RemoteInput.Builder(Constant.EXTRA_VOICE_REPLY)
-                .setLabel("Choose to open")
-                .setChoices(replyChoices)
-                .build();
-        Intent voiceReplyIntent = new Intent(this, RouterActivity.class);
-        PendingIntent voicePendingIntent = PendingIntent.getActivity(this, 0, voiceReplyIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-        NotificationCompat.Action replyAction =
-                new NotificationCompat.Action.Builder(
-                        R.drawable.mobile_small,
-                        "Choose action",
-                        voicePendingIntent)
-                .addRemoteInput(remoteInput)
-                .build();
-
-
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
-                .setSmallIcon(R.drawable.mobile_small)
-                .setContentTitle("Points exchange")
-                .setContentText("Use points to exchange gift in CMCC shop.")
-                .setContentIntent(pendingIntent)
-                .addAction(R.drawable.mobile_small, "Navigate to CMCC", actionPendingIntent)
-                .extend(new NotificationCompat.WearableExtender().addAction(replyAction));
-        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
-        notificationManager.notify(notificationID, builder.build());
-    }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
--- mobile/src/main/java/net/gongmingqm10/cmcc/jpush/JPushReceiver.java
+++ mobile/src/main/java/net/gongmingqm10/cmcc/jpush/JPushReceiver.java
@@ -25,11 +25,6 @@ public class JPushReceiver extends BroadcastReceiver {
         if (JPushInterface.ACTION_MESSAGE_RECEIVED.equals(intent.getAction())) {
             String message = bundle.getString(JPushInterface.EXTRA_MESSAGE);
             startNotification(message, context);
-//            String extras = bundle.getString(JPushInterface.EXTRA_EXTRA);
-//            Intent msgIntent = new Intent(Constant.MESSAGE_RECEIVED_ACTION);
-//            msgIntent.putExtra(Constant.KEY_MESSAGE, message);
-//            if (!TextUtils.isEmpty(extras)) msgIntent.putExtra(Constant.KEY_EXTRAS, extras);
-//            context.sendBroadcast(msgIntent);
         }
     }
 
@@ -38,32 +33,18 @@ public class JPushReceiver extends BroadcastReceiver {
         Intent notificationIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://10086.cn/sn/"));
         PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
 
-        Intent actionIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:0,0?q=中国移动"));
+//        Intent actionIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:0,0?q=中国移动"));
+        Intent actionIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("google.navigation:q=中国移动"));
         PendingIntent actionPendingIntent = PendingIntent.getActivity(context, 0, actionIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-
-
-        String[] replyChoices = new String[] {Constant.CMD_YES, Constant.CMD_NO, Constant.CMD_MAYBE};
-        RemoteInput remoteInput = new RemoteInput.Builder(Constant.EXTRA_VOICE_REPLY)
-                .setLabel("Choose to open")
-                .setChoices(replyChoices)
-                .build();
-        Intent voiceReplyIntent = new Intent(context, RouterActivity.class);
-        PendingIntent voicePendingIntent = PendingIntent.getActivity(context, 0, voiceReplyIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-        NotificationCompat.Action replyAction =
-                new NotificationCompat.Action.Builder(
-                        R.drawable.mobile_small,
-                        "Choose action",
-                        voicePendingIntent)
-                        .addRemoteInput(remoteInput)
-                        .build();
 
 
         NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
                 .setSmallIcon(R.drawable.mobile_small)
+                .setContentTitle(context.getString(R.string.app_name))
                 .setContentText(message)
                 .setContentIntent(pendingIntent)
-                .addAction(R.drawable.mobile_small, "Navigate to CMCC", actionPendingIntent)
-                .extend(new NotificationCompat.WearableExtender().addAction(replyAction));
+                .addAction(R.drawable.mobile_small, "Navigate to CMCC", actionPendingIntent);
+
         NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
         notificationManager.notify(notificationID, builder.build());
     }
